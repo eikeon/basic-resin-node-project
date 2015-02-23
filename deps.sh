36b7f486c6dda00f69cf818da42da3064567ab4a
Daniel Krech <eikeon@eikeon.com>
2015-02-23 11:47:25 -0500

    Attempt at installing and running avahi-daemon

--- deps.sh
+++ deps.sh
@@ -3,5 +3,6 @@
 set -o errexit
 
 apt-get -q update
-apt-get install -y cowsay
+apt-get install -y cowsay avahi-daemon
+/etc/init.d/avahi-daemon restart
 /usr/games/cowsay "Install dependencies using a script like this!"
