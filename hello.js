46af2fdd71c7ef95fc26a62ceb19331c71b29ba2
Daniel Krech <eikeon@eikeon.com>
2015-02-23 11:44:51 -0500

    Added code for testing DNS lookups

--- hello.js
+++ hello.js
@@ -1,1 +1,11 @@
-console.log('hi')
+console.log('hi');
+
+var dns = require('dns');
+
+dns.lookup('www.google.com', function onLookup(err, addresses, family) {
+  console.log('addresses:', addresses);
+});
+
+dns.lookup('marvin.local', function onLookup(err, addresses, family) {
+  console.log('addresses:', addresses);
+});
